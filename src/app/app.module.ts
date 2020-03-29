5741babffd2c37945caa0ffc20548de836e1f79d
zacnm <zackmarsden@hotmail.co.uk>
2020-03-29 14:25:57 +0100

    Add basic layout components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SiteContentContainerComponent } from './components/site-content-container/site-content-container.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SiteContentContainerComponent,
+    SidebarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/site-content-container/site-content-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-site-content-container',
+  templateUrl: './site-content-container.component.html',
+  styleUrls: ['./site-content-container.component.scss']
+})
+export class SiteContentContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
