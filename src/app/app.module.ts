c6439e96dcb8f1d8b4c02aab2bb3b63bc0a0611d
zacnm <zackmarsden@hotmail.co.uk>
2020-03-31 19:54:36 +0100

    Implement flex layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { SiteContentContainerComponent } from './components/site-content-container/site-content-container.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MainComponent } from './components/main/main.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    SiteContentContainerComponent,
     SidebarComponent,
-    HomeComponent
+    HomeComponent,
+    MainComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
